0bb1893f6544715cb4df23d40cfe0a3f7d6b449e
rjsteinert <rj@rjsteinert.com>
2018-10-26 10:31:41 -0400

    Add missing helpers.js

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+
+
+// From https://stackoverflow.com/a/12646864
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array
+}
+
+export { shuffleArray }
